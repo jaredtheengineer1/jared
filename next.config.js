464ae22b5faec50dd8d0bc1d8984948d0dec5e99
Jared Clayborn <jared.the.engineer1@gmail.com>
2026-01-04 19:28:23 -0700

    update for vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-const repo = process.env.GITHUB_REPOSITORY?.split("/")?.[1] ?? "";
-const isPages = process.env.GITHUB_PAGES === "true";
-const basePath = isPages && repo ? `/${repo}` : "";
-const assetPrefix = basePath || "";
+// const repo = process.env.GITHUB_REPOSITORY?.split("/")?.[1] ?? "";
+// const isPages = process.env.GITHUB_PAGES === "true";
+// const basePath = isPages && repo ? `/${repo}` : "";
+// const assetPrefix = basePath + "/" || "";
 
 module.exports = {
-  output: "export",
-  trailingSlash: true,
-  basePath,
-  assetPrefix,
+  // output: "export",
+  // trailingSlash: true,
+  // basePath,
+  // assetPrefix,
 };
