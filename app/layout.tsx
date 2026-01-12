b55a3236bfca5c5465a5b36831be4ebad432ad0d
Jared Clayborn <jared.the.engineer1@gmail.com>
2026-01-11 20:14:35 -0700

    update import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Nav } from "./components/Nav/Nav";
-import "jaredsdesignsystem/dist/index.css";
+import "@jared.the.engineer1/design-system/dist/index.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
